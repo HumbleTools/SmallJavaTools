c688dd5576df5b30e509ed70c1c574bce3dc6613
louis madeuf <louis.madeuf@gfi.fr>
2015-04-29 12:12:26 +0200

    init project

---
+++ src/CSVParser.java
@@ -0,0 +1,235 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * CSVParser est un utilitaire permettant de lire un fichier CSV depuis un cheminFichier fourni. CSVParser se base sur la classe java.util.Scanner pour lire le
+ * fichier et présenter ligne par ligne son contenu au code utilisant un CSVParser. CSVParser ne conservera pas de référence sur chaque ligne lue et il ne faut
+ * pas garder chaque ligne en mémoire quand on utilise CSVParser : cela permet de ne pas saturer la mémoire et ainsi de lire des fichiers très lourds,
+ * contrairement au com.atos.awl.pht.has.sara.batch.sigaes.tools.CsvReader qui ne supportera que de petits fichiers. L'encodage utilisé par défaut est
+ * UTF-8.</br></br> Cas d'utilisation : </br> - Instancier un CSVParser via l'un des constructeurs</br> - Dans une boucle while (appeler hasNextLine())
+ * récupérer chaque ligne sans conserver de référence en dehors de la boucle</br> - Quand on sort de la boucle, les ressources sont clôturées automatiquement
+ * 
+ * @author lmadeuf - 06/2014
+ */
+public class CSVParser {
+
+
+	private final FileInputStream inputStream;
+	private final Scanner scanner;
+	private final String filePath;
+	private final String charset;
+
+	private Character separator;
+	private Long numberOfLinesRead;
+
+	/**
+	 * Séparateur CSV par défaut correspondant à la locale fr
+	 */
+	public static final Character DEFAULT_CSV_SEPARATOR_FR = ';';
+
+	/**
+	 * Charset utilisé par défaut par un CSVParser s'il n'est pas spécifié au constructeur
+	 */
+	public static final String DEFAULT_CHARSET = "CP1252"; // CP1252 => plus connu sous le nom ANSI
+
+	/**
+	 * Crée un CSVParser avec le fichier spécifié par le filePath, le séparateur ';' par défaut et l'encodage par défaut DEFAULT_CHARSET
+	 * 
+	 * @throws FileNotFoundException si le fichier spécifié n'existe pas
+	 */
+	public CSVParser(final String filePath) throws FileNotFoundException {
+		this(filePath, DEFAULT_CHARSET, DEFAULT_CSV_SEPARATOR_FR);
+	}
+
+	/**
+	 * Crée un CSVParser avec le fichier spécifié par le filePath, le séparateur ';' par défaut et l'encodage charset
+	 * 
+	 * @throws FileNotFoundException si le fichier spécifié n'existe pas
+	 */
+	public CSVParser(final String filePath, final String charset) throws FileNotFoundException {
+		this(filePath, charset, DEFAULT_CSV_SEPARATOR_FR);
+	}
+
+	/**
+	 * Crée un CSVParser avec le fichier spécifié par le filePath, le séparateur csvSeparator et l'encodage par défaut DEFAULT_CHARSET
+	 * 
+	 * @throws FileNotFoundException si le fichier spécifié n'existe pas
+	 */
+	public CSVParser(final String filePath, final Character csvSeparator) throws FileNotFoundException {
+		this(filePath, DEFAULT_CHARSET, csvSeparator);
+	}
+
+	/**
+	 * Crée un CSVParser avec le fichier spécifié par le filePath, le séparateur csv csvSeparator et l'encodage charset
+	 * 
+	 * @throws FileNotFoundException si le fichier spécifié n'existe pas
+	 */
+	public CSVParser(final String filePath, final String charset, final Character csvSeparator) throws FileNotFoundException {
+		if(charset==null){
+			throw new IllegalArgumentException("L'encodage du fichier doit être spécifié !");
+		}
+		if(filePath == null){
+			throw new IllegalArgumentException("L'argument filePath doit être spécifié !");
+		}
+		if (csvSeparator == null) {
+			throw new IllegalArgumentException("L'argument csvSeparator soir être renseigné !");
+		}
+		this.filePath = filePath;
+		this.charset = charset;
+		separator = csvSeparator;
+		inputStream = new FileInputStream(filePath);
+		scanner = new Scanner(inputStream, charset);
+		numberOfLinesRead = 0L;
+	}
+
+	/**
+	 * Permet de lire la ligne suivante dans le fichier et de la retourner sous forme de String[]. Si on cherche à lire la première ligne et que le séparateur
+	 * CSV est spécifié par le fichier, alors ce séparateur sera utilisé pour parser le fichier CSV. Dans ce cas, la méthode retournera directement la seconde
+	 * ligne.
+	 * 
+	 * @throws IOException - Si une erreur I/O survient
+	 * @throws IllegalStateException - Si le fichier ne contient pas de données
+	 */
+	public String[] getNextLineOfCells() throws IOException, IllegalStateException {
+		String[] cellulesRetour = null;
+
+		try {
+			String ligneCSV = scanner.nextLine();
+			numberOfLinesRead++;
+			if (ligneCSV != null) {
+				if ((numberOfLinesRead == 1L) && ligneCSV.contains("sep=") && (ligneCSV.length() == 5)) {
+					separator = ligneCSV.charAt(4);
+					if (scanner.hasNextLine()) {
+						ligneCSV = scanner.nextLine();
+						numberOfLinesRead++;
+					} else {
+						closeResources();
+						throw new IllegalStateException("Le fichier CSV ne contient pas de données !");
+					}
+				}
+				cellulesRetour = ligneCSV.split(separator.toString());
+			}
+		} catch (final IllegalStateException ise) {
+			closeResources();
+			throw ise;
+		} catch (final IOException ioe) {
+			closeResources();
+			throw ioe;
+		}
+
+		return cellulesRetour;
+	}
+
+	/**
+	 * Renvoie true si le fichier lu contient encore une ligne après celle que l'on vient de lire. Renvoie false si on a atteint la fin du fichier. Cette
+	 * méthode cloture automatiquement les ressources utilisées.
+	 * 
+	 * @throws IllegalStateException si les ressources sont fermées
+	 * @throws IOException si une erreur I/O survient
+	 */
+	public boolean hasNextLine() throws IOException {
+		boolean hasNextLine = true;
+		try {
+			hasNextLine = scanner.hasNextLine();
+			if (!hasNextLine) {
+				closeResources();
+			}
+		} catch (final IllegalStateException ise) {
+			closeResources();
+			hasNextLine = false;
+		} catch (final IOException ioe) {
+			closeResources();
+			throw ioe;
+		}
+		return hasNextLine;
+	}
+
+	private void closeResources() throws IOException {
+		if (inputStream != null) {
+			inputStream.close();
+		}
+		if (scanner != null) {
+			scanner.close();
+		}
+	}
+
+	/**
+	 * Retourne le chemin vers le fichier utilisé par ce CSVParser
+	 */
+	public String getFilePath() {
+		return filePath;
+	}
+
+	/**
+	 * Retourne le charset utilisé par ce CSVParser
+	 */
+	public String getCharset() {
+		return charset;
+	}
+
+	/**
+	 * Retourne le séparateur CSV utilisé par ce CSVParser
+	 */
+	public Character getSeparator() {
+		return separator;
+	}
+
+	/**
+	 * Retourne le nombre de lignes lues par le parseur
+	 */
+	public Long getNumberOfLinesRead() {
+		return numberOfLinesRead;
+	}
+
+	/**
+	 * Permet de comparer une ligne CSV avec un résultat attendu. On peut préciser si l'on souhaite trimmer les valeurs, si on veut gérer la casse et si l'on
+	 * souhaite nettoyer la chaîne de caractères avec le pattern proposé avant de comparer.
+	 * 
+	 * @param expected les valeurs attendues
+	 * @param values les valeurs lues
+	 * @param trim mettre à vrai si on veut trimmer les chaînes avant comparaison
+	 * @param putToLowercase mettre à vrai si l'on veut mettre à la casse basse avant comparaison
+	 * @param removeUnsafeCharacters retire les caractères avec ce pattern regex [âäàêëéèöôùüûïîç&#@%',_\\t\\s]
+	 * @return vrai si les tableaux contiennent des valeurs similaires, faux sinon
+	 */
+	public static boolean isValeursIdentiques(final String[] expected, final String[] values, final boolean trim, final boolean putToLowercase,
+			final boolean removeUnsafeCharacters) {
+		boolean isStructureCorrecte = true;
+		if ((expected == null) || (values == null) || (expected.length != values.length)) {
+			isStructureCorrecte = false;
+		} else {
+			for (int i = 0; i < expected.length; i++) {
+				if ((expected[i] == null) || (values[i] == null)) {
+					isStructureCorrecte = false;
+					break;
+				} else {
+					String expectedValue = new String(expected[i]);
+					String value = new String(values[i]);
+
+					if (trim) {
+						expectedValue = expectedValue.trim();
+						value = value.trim();
+					}
+					if (putToLowercase) {
+						expectedValue = expectedValue.toLowerCase();
+						value = value.toLowerCase();
+					}
+					if (removeUnsafeCharacters) {
+						final String regexPattern = "[âäàêëéèöôùüûïîç&#@%',_\\t\\s]";
+						final String replacement = "";
+						expectedValue = expectedValue.replaceAll(regexPattern, replacement);
+						value = value.replaceAll(regexPattern, replacement);
+					}
+					if (!expectedValue.equalsIgnoreCase(value)) {
+						isStructureCorrecte = false;
+						break;
+					}
+				}
+			}
+		}
+		return isStructureCorrecte;
+	}
+
+}
---
+++ src/StringOperation.java
@@ -0,0 +1,298 @@
+import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/* ==========================================================================================
+ * repris depuis http://java.developpez.com/sources/?page=nombresDatesString#supprimmerAccents
+ * ========================================================================================== */
+
+
+/** Classe complementaire du J2SDK sur la manipulation de chaines de caractéres
+ *  Permet nottament de supprimer les accents d'une chaine de caractères
+ *
+ *  Avantage :
+ *   - Conserve la casse Majuscule / Minuscule
+ *   - Transforme certains caractères spéciaux tel que ÷ en /
+ *   - Gére le bi-caractères tels que Æ en AE
+ *   - La transformation est efficace en determinant si le caractère est considéré avec accent ou non.
+ *
+ *
+ *  Inconvenient :
+ *   - la taille de la chaine résultat peut ne pas faire la même taille que la source. nottament a cause de Æ et æ
+ *   - Certains caractères spéciaux sont transformé même si ce n'etait pas voulu.
+ *   
+ *   L'utilisation est simple :
+ *   String chaine   = "Accés à la base";
+ *   String chaine2 = StringOperation.sansAccent(chaine);  
+ */
+public abstract class StringOperation
+{
+	/** Index du 1er caractere accentué **/
+	private static final int MIN = 192;
+	/** Index du dernier caractere accentué **/
+	private static final int MAX = 255;
+	/** Index du premier chiffre**/
+	private static final int MIN_NB = 48;
+	/** Index du dernier chiffre**/
+	private static final int MAX_NB = 57;
+	/** Index de la premiere majuscule**/
+	private static final int MIN_MAJ = 65;
+	/**Index de la derniere majuscule**/
+	private static final int MAX_MAJ = 90;
+	/** Index de la premiere minuscule**/
+	private static final int MIN_MIN = 97;
+	/**Index de la derniere minuscule**/
+	private static final int MAX_MIN = 122;
+	/**Index de l'espace**/
+	private static final int SPACE_VALUE = 32;
+	/** Vecteur de correspondance entre accent / sans accent **/
+	private static final Vector<String> map = initMap();
+	/** Regexp : retours à la ligne */
+	private static final String REGEXP_LINERET = "[\\r\\n]+";
+	/** Chaine de caractères : simple espace */
+	private static final String SPACE = " ";
+	
+	/** Initialisation du tableau de correspondance entre les caractéres accentués
+	 * et leur homologues non accentués 
+	 * @return
+	 */
+	private static Vector<String> initMap() {  
+		Vector<String> Result         = new Vector<String>();
+		java.lang.String car  = null;
+		
+		car = new java.lang.String("A");
+		Result.add( car );            /* '\u00C0'   À   alt-0192  */ 
+		Result.add( car );            /* '\u00C1'   Á   alt-0193  */
+		Result.add( car );            /* '\u00C2'   Â   alt-0194  */
+		Result.add( car );            /* '\u00C3'   Ã   alt-0195  */
+		Result.add( car );            /* '\u00C4'   Ä   alt-0196  */
+		Result.add( car );            /* '\u00C5'   Å   alt-0197  */
+		car = new java.lang.String("AE");
+		Result.add( car );            /* '\u00C6'   Æ   alt-0198  */
+		car = new java.lang.String("C");
+		Result.add( car );            /* '\u00C7'   Ç   alt-0199  */
+		car = new java.lang.String("E");
+		Result.add( car );            /* '\u00C8'   È   alt-0200  */
+		Result.add( car );            /* '\u00C9'   É   alt-0201  */
+		Result.add( car );            /* '\u00CA'   Ê   alt-0202  */
+		Result.add( car );            /* '\u00CB'   Ë   alt-0203  */
+		car = new java.lang.String("I");
+		Result.add( car );            /* '\u00CC'   Ì   alt-0204  */
+		Result.add( car );            /* '\u00CD'   Í   alt-0205  */
+		Result.add( car );            /* '\u00CE'   Î   alt-0206  */
+		Result.add( car );            /* '\u00CF'   Ï   alt-0207  */
+		car = new java.lang.String("D");
+		Result.add( car );            /* '\u00D0'   Ð   alt-0208  */
+		car = new java.lang.String("N");
+		Result.add( car );            /* '\u00D1'   Ñ   alt-0209  */
+		car = new java.lang.String("O");
+		Result.add( car );            /* '\u00D2'   Ò   alt-0210  */
+		Result.add( car );            /* '\u00D3'   Ó   alt-0211  */
+		Result.add( car );            /* '\u00D4'   Ô   alt-0212  */
+		Result.add( car );            /* '\u00D5'   Õ   alt-0213  */
+		Result.add( car );            /* '\u00D6'   Ö   alt-0214  */
+		car = new java.lang.String("");
+		Result.add( car );            /* '\u00D7'   ×   alt-0215  */
+		car = new java.lang.String("0");
+		Result.add( car );            /* '\u00D8'   Ø   alt-0216  */
+		car = new java.lang.String("U");
+		Result.add( car );            /* '\u00D9'   Ù   alt-0217  */
+		Result.add( car );            /* '\u00DA'   Ú   alt-0218  */
+		Result.add( car );            /* '\u00DB'   Û   alt-0219  */
+		Result.add( car );            /* '\u00DC'   Ü   alt-0220  */
+		car = new java.lang.String("Y");
+		Result.add( car );            /* '\u00DD'   Ý   alt-0221  */
+		car = new java.lang.String("p");
+		Result.add( car );            /* '\u00DE'   Þ   alt-0222  */
+		car = new java.lang.String("ss");
+		Result.add( car );            /* '\u00DF'   ß   alt-0223  */
+		car = new java.lang.String("a");
+		Result.add( car );            /* '\u00E0'   à   alt-0224  */
+		Result.add( car );            /* '\u00E1'   á   alt-0225  */
+		Result.add( car );            /* '\u00E2'   â   alt-0226  */
+		Result.add( car );            /* '\u00E3'   ã   alt-0227  */
+		Result.add( car );            /* '\u00E4'   ä   alt-0228  */
+		Result.add( car );            /* '\u00E5'   å   alt-0229  */
+		car = new java.lang.String("ae");
+		Result.add( car );            /* '\u00E6'   æ   alt-0230  */
+		car = new java.lang.String("c");
+		Result.add( car );            /* '\u00E7'   ç   alt-0231  */
+		car = new java.lang.String("e");
+		Result.add( car );            /* '\u00E8'   è   alt-0232  */
+		Result.add( car );            /* '\u00E9'   é   alt-0233  */
+		Result.add( car );            /* '\u00EA'   ê   alt-0234  */
+		Result.add( car );            /* '\u00EB'   ë   alt-0235  */
+		car = new java.lang.String("i");
+		Result.add( car );            /* '\u00EC'   ì   alt-0236  */
+		Result.add( car );            /* '\u00ED'   í   alt-0237  */
+		Result.add( car );            /* '\u00EE'   î   alt-0238  */
+		Result.add( car );            /* '\u00EF'   ï   alt-0239  */
+		car = new java.lang.String("d");
+		Result.add( car );            /* '\u00F0'   ð   alt-0240  */
+		car = new java.lang.String("n");
+		Result.add( car );            /* '\u00F1'   ñ   alt-0241  */
+		car = new java.lang.String("o");
+		Result.add( car );            /* '\u00F2'   ò   alt-0242  */
+		Result.add( car );            /* '\u00F3'   ó   alt-0243  */
+		Result.add( car );            /* '\u00F4'   ô   alt-0244  */
+		Result.add( car );            /* '\u00F5'   õ   alt-0245  */
+		Result.add( car );            /* '\u00F6'   ö   alt-0246  */
+		car = new java.lang.String("");
+		Result.add( car );            /* '\u00F7'   ÷   alt-0247  */
+		car = new java.lang.String("o");
+		Result.add( car );            /* '\u00F8'   ø   alt-0248  */
+		car = new java.lang.String("u");
+		Result.add( car );            /* '\u00F9'   ù   alt-0249  */
+		Result.add( car );            /* '\u00FA'   ú   alt-0250  */
+		Result.add( car );            /* '\u00FB'   û   alt-0251  */
+		Result.add( car );            /* '\u00FC'   ü   alt-0252  */
+		car = new java.lang.String("y");
+		Result.add( car );            /* '\u00FD'   ý   alt-0253  */
+		car = new java.lang.String("p");
+		Result.add( car );            /* '\u00FE'   þ   alt-0254  */
+		car = new java.lang.String("y");
+		Result.add( car );            /* '\u00FF'   ÿ   alt-0255  */
+		Result.add( car );            /* '\u00FF'       alt-0255  */
+		
+		return Result;
+	}
+	
+	/** Transforme une chaine pouvant contenir des accents dans une version sans accent
+	 *  @param chaine Chaine a convertir sans accent
+	 *  @return Chaine dont les accents ont été supprimé
+	 **/
+	public static java.lang.String sansAccent(java.lang.String chaine) {  
+		java.lang.StringBuffer Result  = new StringBuffer(chaine);
+		
+		for(int bcl = 0 ; bcl < Result.length() ; bcl++) { 
+			int carVal = chaine.charAt(bcl);
+			if( carVal >= MIN && carVal <= MAX ) {  // Remplacement
+				java.lang.String newVal = (java.lang.String) map.get( carVal - MIN );
+				Result.replace(bcl, bcl+1,newVal);
+			}
+			// Cas du caractere °
+			if(carVal == 176) Result.replace(bcl, bcl+1, " ");
+		}
+		return Result.toString();
+	}
+	
+	/** Transforme une chaine pouvant contenir des accents dans une version sans accents et sans espaces.
+	 *  @param chaine Chaine a convertir sans accent et sans espaces
+	 *  @return Chaine dont les accents et les espaces ont été supprimé
+	 **/
+	public static java.lang.String sansAccentNiEspaces(java.lang.String chaine) {  
+		java.lang.StringBuffer Result  = new StringBuffer(chaine);
+		
+		for(int bcl = 0 ; bcl < Result.length() ; bcl++) { 
+			int carVal = chaine.charAt(bcl);
+			if( carVal >= MIN && carVal <= MAX ) {  // Remplacement
+				java.lang.String newVal = (java.lang.String) map.get( carVal - MIN );
+				Result.replace(bcl, bcl+1,newVal);
+			}
+			//Cas de l'espace
+			if(carVal == SPACE_VALUE) Result.replace(bcl, bcl+1, "_");
+		}
+		return Result.toString();
+	}
+
+
+	public static String replaceAll(String ch, String replaceWhatPattern, String replaceWith) {
+		Pattern pattern = Pattern.compile(replaceWhatPattern);
+		Matcher matcher = pattern.matcher(ch);
+		StringBuffer sb = new StringBuffer();
+		while(matcher.find())
+			matcher.appendReplacement(sb, replaceWith);
+		matcher.appendTail(sb);
+		return sb.toString();
+	}
+	
+	public static String firstCharUpperCase(String ch) {
+		if (ch == null) return null;
+		if (ch.length() == 0) return ch;
+		if (ch.length() == 1) return ch.toUpperCase();
+		return ch.substring(0, 1).toUpperCase() + ch.substring(1);
+	}
+
+	public static String firstCharLowerCase(String ch) {
+		if (ch == null) return null;
+		if (ch.length() == 0) return ch;
+		if (ch.length() == 1) return ch.toLowerCase();
+		return ch.substring(0, 1).toLowerCase() + ch.substring(1);
+	}
+	
+	
+	
+	/**
+	 * getSimpleClassName("pack1.pack2.MaClasse") renvoie "MaClasse".
+	 * Ne renvoie pas null mais la chaîne vide si ch est null ou se termine par un point. 
+	 */
+	public static String getSimpleClassName(String ch) {
+		if (ch == null) return "";
+		int i = ch.lastIndexOf(".");
+		if (i < 0) return ch;
+		if (++i >= ch.length()) return "";
+		return ch.substring(i);
+	}
+	
+	/**
+	 * getPackageName("pack1.pack2.MaClasse") renvoie "pack1.pack2".
+	 * Ne renvoie pas null mais la chaîne vide si ch est null ou ne contient pas de point. 
+	 */
+	public static String getPackageName(String ch) {
+		if (ch == null) return "";
+		int i = ch.lastIndexOf(".");
+		if (i < 0) return "";
+		return ch.substring(0, i);
+	}
+	
+	public static String removeNonAlphanumericOrUnderscoreOrDot(String ch) {
+		Pattern pattern = Pattern.compile("[^a-zA-Z0-9_\\.]");
+		Matcher matcher = pattern.matcher(ch);
+		StringBuffer sb = new StringBuffer();
+		while(matcher.find())
+			matcher.appendReplacement(sb, "_");
+		matcher.appendTail(sb);
+		return sb.toString();
+	}
+
+	// mantis 2281 : identifiant sans caracteres speciaux et espace
+	public static String removeNonAlphanumeric(String ch) {
+		Pattern pattern = Pattern.compile("[^a-zA-Z0-9]");
+		Matcher matcher = pattern.matcher(ch);
+		StringBuffer sb = new StringBuffer();
+		while(matcher.find())
+			matcher.appendReplacement(sb, " ");
+		matcher.appendTail(sb);
+		return sb.toString();
+	}
+
+	/**
+	 * Renvoie la chaine de caractère avec les retours à la ligne remplacés par des espaces.<br/>
+	 * Si la chaine en entrée est <code>null</code>, retourne une chaine vide (non-<code>null</code>)
+	 * @param string la chaine à remplacer
+	 * @return la chaine avec les retours à la ligne remplacés par des espaces
+	 */
+	public static String toSingleLine(String string) {
+		if (string == null) return "";
+		return string.replaceAll(REGEXP_LINERET, SPACE);
+	}
+	
+	/**
+	 * Changes first character of every word to upper case
+	 */
+	public static String firstLetterToUpperCase(String str)
+	{
+		Pattern spaces = Pattern.compile("\\s+[a-z]");       
+		Matcher matcher = spaces.matcher(str);    
+		StringBuilder capitalWordBuilder = new StringBuilder(str.substring(0,1).toUpperCase());
+		int prevStart=1;
+        while(matcher.find()) 
+        {
+            capitalWordBuilder.append(str.substring(prevStart, matcher.end()-1));
+            capitalWordBuilder.append(str.substring(matcher.end()-1, matcher.end()).toUpperCase());
+            prevStart = matcher.end();
+        }   
+        capitalWordBuilder.append(str.substring(prevStart, str.length()));
+        return capitalWordBuilder.toString();
+	}
+}
